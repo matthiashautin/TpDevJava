99648cfb35dbf2ca7896ff147fdbf9707fa4738c
matthiashautin <matthiashautin@gmail.com>
2023-10-10 22:18:14 +0200

    JavaDoc Room, Game

--- zuut/zuut-v4-7.9/Game.java
+++ zuut/zuut-v4-7.9/Game.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 /**
  * Classe Game - le moteur du jeu d'aventure Zuul.
  *
- * @author votre nom
+ * @author Hautin Matthias
  */
 public class Game
 {
     private Room aCurrentRoom;
     private Parser aParser;
     
+    /**
+     * call the createRooms() procedure to create all the rooms and the exits of each room
+     * call play() procedure to start the game
+     * @method Game()
+     */
     public Game() {
         createRooms();
         play();
     } //Game()
 
-        /**
+    /**
      * create all rooms for the game with calling class Room and create the all directions for each rooms
      * @method procedure createRooms()
-     * @params aihfhi
-     * 
      */
     private void createRooms() {
         //Main :
@@ -113,44 +116,59 @@ public class Game
 
     } // createRooms()
     
+    /**
+     * call printWelcome() method 
+     * allows you to analyze the different commands entered by the player
+     * at each loop then the getCommand() method of the parser is called to obtain the next command entered by the player
+     * Then, the processCommand() method is called with the command obtained as a parameter. (example: "go", "quit", "help") and performs appropriate actions.
+     * If the quit command is entered then the game ends and displays "Thank you for playing. Good bye."
+     * @method play()
+     * 
+     */
     private void play() {
-        printWelcome();
-        boolean vFinished = false;
-        this.aParser = new Parser(); // Créer l'objet Parser
+        printWelcome();//appel de la methode printWelcome()
+        boolean vFinished = false; //création d'une variable boolean instantiée à false
+        this.aParser = new Parser(); // Créer l'objet Parser pour gérer l'analyse des commandes entrées par le joueur
         while(vFinished == false) {
-            Command vTypeCommand = this.aParser.getCommand();
-            vFinished = processCommand(vTypeCommand);
+            //si le jeu n'est pas fini
+            Command vTypeCommand = this.aParser.getCommand();// Appel de la méthode getCommand() sur l'objet Parser que je viens de créer
+            vFinished = processCommand(vTypeCommand);//Appel de processCommand() avec le paramètre obtenue
             
             if(vFinished == true) {
+                //si le jeu est fini
                 System.out.println("Thank you for playing. Good bye.");
             }
         }
-        
     } // play()
-
+    
+    /**
+     * allows you to change room if this room exist c_a_d if the direction of the next room is not equal to null
+     * @method goRoom()
+     * @param pCommand type Command allows the player to retrieve the print command
+     */
     public void goRoom(final Command pCommand) {
 
-        if(! pCommand.hasSecondWord()) {
+        if(! pCommand.hasSecondWord()) {//si le second mot n'existe pas 
             System.out.println("Go Where ?");
             return;
-        } 
+        }
 
-        Room vNextRoom = null;
-        String vDirection = pCommand.getSecondWord();
+        Room vNextRoom = null;//initialise une varible de type Room à nulle
+        String vDirection = pCommand.getSecondWord();//initialise une variable de type String qui est le second mot de notre parametre
+        vNextRoom = this.aCurrentRoom.getExit(vDirection);//donne à la variable vNextRoom la valeur de toutes les sorties qui existe pour la room actuelle
         
-        vNextRoom = this.aCurrentRoom.getExit(vDirection);
-        
-        if (vNextRoom == null) {
+        if (vNextRoom == null) {//si la room suivante n'a pas de direction associée
             System.out.println("There is no door !");
             return;
-        } else {
+        } else {//si on change de room on donne la valeur de aCurrentRoom a notre nouvelle room
             this.aCurrentRoom = vNextRoom;
             printLocationInfo();
         }
     } // goRoom()
     
     /**
-     * printWelcome to creating a new game
+     * printWelcome to print information for start game
+     * call printLocationInfo() to get the description of the first aCurrentRooms now (vMainPilot)
      * @method printWelcome()
      */
     private void printWelcome() {
@@ -160,37 +178,52 @@ public class Game
         printLocationInfo();
     } //printWelcome()
     
+    /**
+     * printHelp to print if you write "help"
+     * @method printHelp()
+     */
     private void printHelp() {
         System.out.println("You are lost. You are alone.\nYou wander around at the ship. \nYour command words are: go quit help");
     } //printHelp()
     
+    /**
+     * quit return true if you write "quit"
+     * @method quit()
+     * @param pSecondMot type Command check secondWord and if there is a second word print "Quit what?"
+     */
     private boolean quit(final Command pSecondMot) {
-        if(pSecondMot.hasSecondWord() == true ) {
+        if(pSecondMot.hasSecondWord() == true ) {//si l'utilisateur tap un second mot aprés "quit" (exemple: "quit program")
             System.out.println("Quit what ?");
             return false;
             
-        } else {
+        } else {//sinon on quit 
             return true;
         }
     } //quit()
     
+    /**
+     * analyse entered command and determines what action must be taken based on this commad and manages the different actions
+     * @method processCommand()
+     * @param pAppelBonneMethod type Command represent the command entered by the player
+     * 
+     */
     private boolean processCommand(final Command pAppelBonneMethod) {
-        if(pAppelBonneMethod.isUnknown() == true) {
+        if(pAppelBonneMethod.isUnknown() == true) { //si la Commande tapée est inconnu
             System.out.println("I don't know what you mean...");
             return false;
         } 
-        else if(pAppelBonneMethod.getCommandWord().equals("quit")) {
+        else if(pAppelBonneMethod.getCommandWord().equals("quit")) { //si la commande tapée est "quit"
              return quit(pAppelBonneMethod);
         } 
-        else if(pAppelBonneMethod.getCommandWord().equals("help")) {
+        else if(pAppelBonneMethod.getCommandWord().equals("help")) { //si la commande tapée est "help"
             printHelp();
             return false;
         } 
-        else if(pAppelBonneMethod.getCommandWord().equals("go")) {
+        else if(pAppelBonneMethod.getCommandWord().equals("go")) { //si la commande tapée est "go"
             goRoom(pAppelBonneMethod);
             return false;
         } 
-        else {
+        else { //si print "Erreur du programmeur : commande non reconnue !"
            System.out.println("Erreur du programmeur : commande non reconnue !");
            return false;
         }   
