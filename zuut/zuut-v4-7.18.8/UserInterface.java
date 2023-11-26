edc32edd44d5ae5055a59367b55ec5b019898469
matthiashautin <matthiashautin@gmail.com>
2023-11-26 18:11:07 +0100

    7.18.8  add button + modif audio

---
+++ zuut/zuut-v4-7.18.8/UserInterface.java
@@ -0,0 +1,164 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import java.awt.image.*;
+
+/**
+ * This class implements a simple graphical user interface with a 
+ * text entry area, a text output area and an optional image.
+ * 
+ * @author Michael Kolling
+ * @version 1.0 (Jan 2003) DB edited (2023)
+ */
+public class UserInterface implements ActionListener
+{
+    private GameEngine aEngine;
+    private JFrame     aMyFrame;
+    private JTextField aEntryField;
+    private JTextArea  aLog;
+    private JLabel     aImage;
+    private JButton    aBtnActiveAudio;
+
+    /**
+     * Construct a UserInterface. As a parameter, a Game Engine
+     * (an object processing and executing the game commands) is
+     * needed.
+     * 
+     * @param gameEngine  The GameEngine object implementing the game logic.
+     */
+    public UserInterface( final GameEngine pGameEngine )
+    {
+        this.aEngine = pGameEngine;
+        this.createGUI();
+    } // UserInterface(.)
+
+    /**
+     * Print out some text into the text area.
+     */
+    public void print( final String pText )
+    {
+        this.aLog.append( pText );
+        this.aLog.setCaretPosition( this.aLog.getDocument().getLength() );
+    } // print(.)
+
+    /**
+     * Print out some text into the text area, followed by a line break.
+     */
+    public void println( final String pText )
+    {
+        this.print( pText + "\n" );
+    } // println(.)
+
+    /**
+     * Show an image file in the interface.
+     */
+    public void showImage( final String pImageName )
+    {
+        String vImagePath = "" + pImageName; // to change the directory
+        URL vImageURL = this.getClass().getClassLoader().getResource( vImagePath );
+        if ( vImageURL == null )
+            System.out.println( "Image not found : " + vImagePath );
+        else {
+            ImageIcon vIcon = new ImageIcon( vImageURL );
+            this.aImage.setIcon( vIcon );
+            this.aMyFrame.pack();
+        }
+    } // showImage(.)
+
+    /**
+     * Enable or disable input in the entry field.
+     */
+    public void enable( final boolean pOnOff )
+    {
+        this.aEntryField.setEditable( pOnOff ); // enable/disable
+        if ( pOnOff ) { // enable
+            this.aEntryField.getCaret().setBlinkRate( 500 ); // cursor blink
+            this.aEntryField.addActionListener( this ); // reacts to entry
+        }
+        else { // disable
+            this.aEntryField.getCaret().setBlinkRate( 0 ); // cursor won't blink
+            this.aEntryField.removeActionListener( this ); // won't react to entry
+        }
+    } // enable(.)
+    
+    /**
+     * Set up graphical user interface.
+     */
+    private void createGUI()
+    {
+        this.aMyFrame = new JFrame( "Crash Ship" ); // change the title !
+        this.aEntryField = new JTextField( 34 );
+
+        this.aLog = new JTextArea();
+        this.aLog.setEditable( false );
+        JScrollPane vListScroller = new JScrollPane( this.aLog );
+        vListScroller.setPreferredSize( new Dimension(250, 250) );
+        vListScroller.setMinimumSize( new Dimension(100,100) );
+        
+        //JButton
+        this.aBtnActiveAudio = new JButton("Active Audio");
+        this.aBtnActiveAudio.setPreferredSize(new Dimension(150,100));
+        
+        this.aImage = new JLabel();
+
+        //temporraire je change la size de l'emplacement de l'image 
+        this.aImage.setPreferredSize(new Dimension(500, 400));
+        
+        JPanel vPanel = new JPanel();
+        vPanel.setLayout( new BorderLayout() ); // ==> only five places
+        vPanel.add( this.aImage, BorderLayout.NORTH );
+        vPanel.add( vListScroller, BorderLayout.CENTER );
+        vPanel.add( this.aEntryField, BorderLayout.SOUTH );
+        vPanel.add( this.aBtnActiveAudio, BorderLayout.EAST);//le placer à l'est
+
+        this.aMyFrame.getContentPane().add( vPanel, BorderLayout.CENTER );
+
+        // add some event listeners to some components
+        this.aEntryField.addActionListener( this );
+        
+        this.aBtnActiveAudio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                aEngine.toggleAudio();
+            }
+        } );
+
+        // to end program when window is closed
+        this.aMyFrame.addWindowListener(
+            new WindowAdapter() { // anonymous class
+                @Override public void windowClosing(final WindowEvent pE)
+                {
+                    System.exit(0);
+                }
+        } );
+     
+        this.aMyFrame.pack();
+        this.aMyFrame.setVisible( true );
+        this.aEntryField.requestFocus();
+    } // createGUI()
+
+    /**
+     * Actionlistener interface for entry textfield.
+     */
+    @Override public void actionPerformed( final ActionEvent pE ) 
+    {
+        // no need to check the type of action at the moment
+        // because there is only one possible action (text input) :
+        this.processCommand(); // never suppress this line
+    } // actionPerformed(.)
+
+    /**
+     * A command has been entered in the entry field.  
+     * Read the command and do whatever is necessary to process it.
+     */
+    private void processCommand()
+    {
+        String vInput = this.aEntryField.getText();
+        this.aEntryField.setText( "" );
+
+        this.aEngine.interpretCommand( vInput );
+    } // processCommand()
+    
+    
+} // UserInterface 
