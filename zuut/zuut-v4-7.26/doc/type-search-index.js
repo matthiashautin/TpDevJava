9a6ac98e96eee61f4698d1c3b8eb8035631a382f
matthiashautin <matthiashautin@gmail.com>
2023-12-08 11:05:57 +0100

    7.26 + javadoc + 7.28

--- zuut/zuut-v4-7.26/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Command"},{"p":"<Unnamed>","l":"CommandWords"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Parser"},{"p":"<Unnamed>","l":"Room"}]
