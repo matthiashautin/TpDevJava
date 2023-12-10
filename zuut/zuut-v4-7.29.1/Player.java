1b6b09cdb762bccce6fc99c36da3f445c48c440c
matthiashautin <matthiashautin@gmail.com>
2023-12-11 00:33:36 +0100

    rapport

--- zuut/zuut-v4-7.29.1/Player.java
+++ zuut/zuut-v4-7.29.1/Player.java
@@ -1,18 +1,18 @@
 import java.util.Stack;
 
 /**
- * Décrivez votre classe Player ici.
+ * player permet la création de nouveaux joueur et aussi d'ennemi
  *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author Matthias Hautin
+ * @version 11/12/2023
  */
 public class Player
 {
-   private String   aNamePlayer;
-   private Room     aCurrentRoom;
+   private String       aNamePlayer;
+   private Room         aCurrentRoom;
    private Stack<Room>  aPreviousRoom;
-   private int      aPoidsMax;
-   private int      aVie;
+   private int          aPoidsMax;
+   private int          aVie;
    
    public Player(final String pNamePlayer, final Room pCurrentRoom , final int pPoidsMax, final int pVie) {
        this.aNamePlayer = pNamePlayer;
@@ -22,27 +22,44 @@ public class Player
        this.aPreviousRoom = new Stack<Room>();
    } //Player()
    
+   /** getter
+    *  return this.aNamePlayer
+    */
    public String getNamePlayer() {
        return this.aNamePlayer;   
    } //getNamePlayer()
    
+   /** getter
+    *  return this.aCurrentRoom
+    */
    public Room getCurrentRoom() {
        return this.aCurrentRoom;
    } //getCurrentRoom()
    
+   /** getter
+    *  return this.aPoidsMax
+    */
    public int getPoidsMax() {
        return this.aPoidsMax;
    } //getPoidsMax()
    
+   /** getter
+    *  return this.aVie
+    */
    public int getVie() {
        return this.aVie;
    } //getVie()
    
-   //setters
+   /** getter
+    *  return this.aPreviousRoom
+    */
    public Stack<Room> getPreviousRoom() {
        return this.aPreviousRoom;
    } //getPreviousRoom()
-   
+    
+   /** setter
+    *  @param pCurrentRoom 
+    */
     public void setCurrentRoom(final Room pCurrentRoom) {
        this.aCurrentRoom = pCurrentRoom;
    } //setCurrentRoom()
